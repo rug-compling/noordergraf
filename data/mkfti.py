0eff3d4e955a58d355059bc93a43739edfa92748
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2021-10-22 14:59:57 +0200

    aanpaste index for free-text search

---
+++ data/mkfti.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+
+AGRAPH_USER     = os.environ.get('AGRAPH_USER')
+AGRAPH_PASSWORD = os.environ.get('AGRAPH_PASSWORD')
+
+from franz.openrdf.connect import ag_connect
+
+conn = ag_connect('noordergraf', create=False, user=AGRAPH_USER, password=AGRAPH_PASSWORD)
+
+conn.createFreeTextIndex("all", predicates=None, indexLiterals=True, indexResources=False, indexFields=["object"], minimumWordSize=2, stopWords=[], wordFilters=["drop-accents"], innerChars=["alphanumeric"], borderChars=None, tokenizer="default")
