60f2e075f488b921db99b1d2a1832842d943937b
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2022-02-25 21:15:30 +0100

    chkll.go: check op :nd zonder :geo en andersom

--- tools/chkll.go
+++ tools/chkll.go
@@ -35,6 +35,8 @@ func main() {
 
 	pattern := "%+08.4f%+09.4f"
 
+	// match tussen :geo en :nd
+
 	query := `
 PREFIX :    <https://noordergraf.rug.nl/ns#>
 PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
@@ -89,6 +91,92 @@ SELECT ?g ?ll ?lat ?lon {
 			fmt.Fprintln(os.Stderr, "ERROR", graph[27:], ll1, lat, lon)
 		}
 	}
+
+	// :geo zonder :nd
+
+	query = `
+PREFIX :    <https://noordergraf.rug.nl/ns#>
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+SELECT ?g ?lat ?lon {
+  GRAPH ?g {
+    ?s :geo ?geo .
+    ?geo geo:lat ?lat .
+    ?geo geo:long ?lon .
+    FILTER NOT EXISTS { ?s :nd ?ll }
+  }
+} ORDER BY ?g
+`
+
+	request = "http://localhost:10035/repositories/noordergraf?infer=false&query=" + url.QueryEscape(query)
+	resp, err = http.Get(request)
+	x(err)
+
+	if resp.StatusCode > 299 {
+		x(fmt.Errorf("Status: %d\n\n%s\n", resp.StatusCode, resp.Status))
+	}
+
+	b, err = ioutil.ReadAll(resp.Body)
+	x(err)
+
+	sparql.Results = sparql.Results[0:0]
+	err = xml.Unmarshal(b, &sparql)
+	x(err)
+
+	for _, result := range sparql.Results {
+		var graph, lat, lon string
+		for _, binding := range result.Bindings {
+			if binding.Name == "g" {
+				graph = binding.URI
+			} else if binding.Name == "lat" {
+				lat = binding.Literal
+			} else if binding.Name == "lon" {
+				lon = binding.Literal
+			}
+		}
+		errors++
+		fmt.Fprintln(os.Stderr, "MISSING :nd", graph[27:], lat, lon)
+	}
+
+	// :nd zonder :geo
+
+	query = `
+PREFIX : <https://noordergraf.rug.nl/ns#>
+SELECT ?g ?ll {
+  GRAPH ?g {
+    ?s :nd ?ll .
+    FILTER NOT EXISTS { ?s :geo ?geo }
+  }
+} ORDER BY ?g
+`
+
+	request = "http://localhost:10035/repositories/noordergraf?infer=false&query=" + url.QueryEscape(query)
+	resp, err = http.Get(request)
+	x(err)
+
+	if resp.StatusCode > 299 {
+		x(fmt.Errorf("Status: %d\n\n%s\n", resp.StatusCode, resp.Status))
+	}
+
+	b, err = ioutil.ReadAll(resp.Body)
+	x(err)
+
+	sparql.Results = sparql.Results[0:0]
+	err = xml.Unmarshal(b, &sparql)
+	x(err)
+
+	for _, result := range sparql.Results {
+		var graph, nd string
+		for _, binding := range result.Bindings {
+			if binding.Name == "g" {
+				graph = binding.URI
+			} else if binding.Name == "nd" {
+				nd = binding.Literal
+			}
+		}
+		errors++
+		fmt.Fprintln(os.Stderr, "MISSING :geo", graph[27:], nd)
+	}
+
 	if errors > 0 {
 		os.Exit(1)
 	}
