5a02bf89fcdea6ff50e4d93b33b0a45c349daa1b
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2022-07-10 15:14:45 +0200

    update ttl2svg.go

--- www/bin/ttl2svg.go
+++ www/bin/ttl2svg.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -38,6 +39,7 @@ var (
 	thingList = make([]*ThingT, 0)
 	types     = make(map[int]string)
 	short     = make([][2]string, 0)
+	skips     = make(map[int]bool)
 )
 
 func main() {
@@ -58,12 +60,28 @@ Missing query
 		return
 	}
 
-	format := "dot"
+	var format, dotDisabled, fdpDisabled, sfdpDisabled string
 	switch req.FormValue("f") {
 	case "fdp":
 		format = "fdp"
+		fdpDisabled = " disabled"
 	case "sfdp":
 		format = "sfdp"
+		sfdpDisabled = " disabled"
+	default:
+		format = "dot"
+		dotDisabled = " disabled"
+	}
+
+	if req.FormValue("a") != "reset" {
+		for _, s := range strings.Split(req.FormValue("s"), "n") {
+			if s != "" {
+				i, err := strconv.Atoi(s)
+				if err == nil {
+					skips[i] = true
+				}
+			}
+		}
 	}
 
 	fp, err := os.Open("/net/noordergraf/data/prefix.ttl")
@@ -123,18 +141,23 @@ Missing query
 	fmt.Fprintln(&buf, "digraph gr {")
 
 	for _, t := range thingList {
-		var e string
-		if t.Type == "literal" {
-			e = ", shape=box"
+		if !skips[t.id] {
+			var e string
+			if t.Type == "literal" {
+				e = ", shape=box"
+			}
+			fmt.Fprintf(&buf, "n%d [label=%q%s];\n", t.id, label(t, true), e)
 		}
-		fmt.Fprintf(&buf, "n%02d [label=%q%s];\n", t.id, label(t, true), e)
 	}
 
 	for _, tri := range data.Triples {
 		if tri.Predicate.Value == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" {
 			continue
 		}
-		fmt.Fprintf(&buf, "n%02d -> n%02d [label=%q];\n", tri.Subject.id, tri.Object.id, label(tri.Predicate, false))
+		if skips[tri.Subject.id] || skips[tri.Object.id] {
+			continue
+		}
+		fmt.Fprintf(&buf, "n%d -> n%d [label=%q];\n", tri.Subject.id, tri.Object.id, label(tri.Predicate, false))
 	}
 
 	fmt.Fprintln(&buf, "}")
@@ -149,18 +172,82 @@ Missing query
 	svg := string(b)
 	svg = svg[strings.Index(svg, "<svg"):]
 
+	skipobj := make([]string, 0, len(skips))
+	for key := range skips {
+		skipobj = append(skipobj, fmt.Sprintf(`"n%d":true`, key))
+	}
+
 	fmt.Printf(`Content-type: text/html; charset=utf-8
 
 <!DOCTYPE html>
 <html>
   <head>
     <title>%s</title>
+    <script src="/jquery.js"></script>
+    <script>
+      var items = {%s};
+      function cl(item) {
+        var id = $(item).find('title').text();
+        if (items[id]) {
+          delete items[id];
+          item.classList.remove("mark");
+        } else {
+          items[id] = true;
+          item.classList.add("mark");
+        }
+      }
+      function doForm() {
+        if (document.forms["myform"]["a"].value != "reset") {
+          var s = "";
+          for (var key in items) {
+            s += key;
+          }
+          document.forms["myform"]["s"].value = s;
+        }
+        return true;
+      }
+    </script>
+    <style>
+      .mark ellipse,
+      .mark polygon {
+        stroke: blue;
+        stroke-width: 3;
+      }
+      .scroll {
+        padding: 1em 0px;
+        margin: 1em 0px;
+        overflow-x: auto;
+      }
+    </style>
   </head>
   <body>
+    <form name="myform" action="https://noordergraf.rug.nl/bin/ttl2svg" onsubmit="return doForm()" method="get">
+      <input type="hidden" name="t" value="%s">
+      <input type="hidden" name="s">
+      Lay-out:
+      <input type="submit" name="f" value="dot"%s>
+      <input type="submit" name="f" value="fdp"%s>
+      <input type="submit" name="f" value="sfdp"%s>
+      <p>
+Klik op tekst in nodes om te selecteren en klik →
+      <input type="submit" name="a" value="verbergen">
+<p>
+      <input type="submit" name="a" value="reset">
+    </form>
+<div class="scroll">
 %s
+</div>
+    <script>
+      $('g.node').on('click', function() { cl(this); });
+    </script>
+TODO:
+<ul>
+<li>Bij klik op "verbergen" of "reset" niet terug naar formaat "dot"
+<li>Ook dochterelementen verbergen, tenzij die een andere parent hebben die niet verborgen is
+</ul>
   </body>
 </html>
-`, html.EscapeString(tfile), svg)
+`, html.EscapeString(tfile), strings.Join(skipobj, ",\n"), tfile, dotDisabled, fdpDisabled, sfdpDisabled, svg)
 
 }
 
